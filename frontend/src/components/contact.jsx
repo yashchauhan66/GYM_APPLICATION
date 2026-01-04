2c61bba060ddfb7b25980abe5776fb2d4463d67c
Your Name <your.email@example.com>
2026-01-04 11:23:07 +0530

    Fix css case sensitive

--- frontend/src/components/contact.jsx
+++ frontend/src/components/contact.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./Contact.css";
+import "./contact.css";
 
 const Contact = () => {
   const navigate = useNavigate();
